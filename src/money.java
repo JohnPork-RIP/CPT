b74b3113b8279551d3cdbfc3d79be8d5480e78b8
John Pork <farishazim908@gmail.com>
2025-01-12 16:46:59 -0500

    finished money

--- src/money.java
+++ src/money.java
@@ -5,10 +5,23 @@
  */
 
 public class money{
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
+
         double expenses = 2300;
         double income = 4000;
         User faris = new User("johnpork", "happyballs1");
+        Transaction t1 = new Transaction(1500.75, "payment", "10/01/2025");
+        Transaction t2 = new Transaction(24500.50, "deposit", "11/10/2025");
+        Transaction t3 = new Transaction(785.99, "withdrawal", "07/15/2025");
+        Transaction t4 = new Transaction(320.00, "purchase", "09/25/2025");
+        Transaction t5 = new Transaction(1900.30, "transfer", "03/12/2025");
+        Transaction t6 = new Transaction(11000.40, "loan", "01/10/2025");
+        Transaction t7 = new Transaction(800.20, "refund", "02/22/2025");
+        Transaction t8 = new Transaction(500.50, "gift", "08/09/2025");
+        Transaction t9 = new Transaction(120.60, "subscription", "06/17/2025");
+        Transaction t10 = new Transaction(2500.90, "savings", "05/05/2025"); // ai im lazy
+
         faris.Login("johnpork", "happyballs1");
         faris.ViewProfile();
         faris.addIncome(income);
@@ -17,8 +30,10 @@ public class money{
         faris.delIncome(income-2500);
         faris.viewIncome();
         faris.viewExpenses();
-
+        Db.findTransaction();
+        
     }
+        
 }
 
 class User extends Account{
@@ -75,7 +90,6 @@ class Account{
         this.income += income;
         checkBalance();
         viewBalance();
-        Db.addTransaction(null);
     }
 
     public void addExpense(double expense){
@@ -114,8 +128,37 @@ class Transaction{
     private String category;
     private String date;
 
-    public Transaction() {
-        // ...amount
+    public Transaction(double amount, String category, String date) {
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
         Db.addTransaction(this);
+    }
+
+
+    public double getAmount(){
+        if (this.amount == 0.0){
+            return 0.0;
+        }
+        return this.amount;
+    }
+    
+    public String getCategory(){
+        if (this.category == null){
+            return "0";
+        }
+        return this.category;
+    }
+
+    public String getDate(){
+        if (this.date == null){
+            return "0";
+        }
+        return this.date;
+    }
+
+    @Override
+    public String toString(){
+        return "Amount: "+this.amount+"\n"+"Category: "+this.category+"\n"+"Date: "+date;
     }
 }
