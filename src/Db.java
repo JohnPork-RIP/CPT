fb31ff8bc1cdd868a9de6bbd462ecebbd0f03ff7
JohnPork-RIP <farishazim908@gmail.com>
2025-01-13 08:47:47 -0800

    finished storing transactions

--- src/Db.java
+++ src/Db.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public final class Db {
@@ -7,28 +6,27 @@ public final class Db {
         Transaction t1 = new Transaction(1500.75, "payment", "10/01/2025");
         Transaction t2 = new Transaction(24500.50, "deposit", "11/10/2025");
         Transaction t3 = new Transaction(785.99, "withdrawal", "07/15/2025");
-        addTransaction(t1);
-        addTransaction(t2);
-        addTransaction(t3);
-        System.out.println(transactions.length);
+
+        for (Transaction string : transactions) {
+            System.out.println(string);
+        }
+        
     }
-    protected static Transaction[] transactions = {};
+    protected static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
     public static void addTransaction(Transaction t) {
-        ArrayList<Transaction> list = new ArrayList<Transaction>(Arrays.asList(transactions));
-        list.add(t);
-        transactions = list.toArray(new Transaction[list.size()]);
+        transactions.add(t);
     }
 
-    public static int searchTransactionA(Transaction[] x, Double a){
+    public static int searchTransactionA(ArrayList<Transaction> x, Double a){
         int low = 0;
-        int high = x.length - 1;
+        int high = x.size() - 1;  // size
 
         while (high >= low) {
             int middle = (low + high) / 2;
-            if (x[middle].getAmount() == a) {
+            if (x.get(middle).getAmount() == a) {  // x.get(middle)
                 return middle;
-            } else if (x[middle].getAmount() < a) {
+            } else if (x.get(middle).getAmount() < a) {
                 low = middle + 1;
             } else {
                 high = middle - 1;
@@ -37,15 +35,32 @@ public final class Db {
         return -1;
     }
 
-    public static int searchTransactionCD(Transaction[] x, String c){
+    public static int searchTransactionC(ArrayList<Transaction> x, String c){
         int low = 0;
-        int high = x.length - 1;
+        int high = x.size() - 1;
 
         while (high >= low) {
             int middle = (low + high) / 2;
-            if (x[middle].getCategory().compareTo(c) == 0) {
+            if (x.get(middle).getCategory().compareTo(c) == 0) {
                 return middle;
-            } else if (x[middle].getCategory().compareTo(c) < 0) {
+            } else if (x.get(middle).getCategory().compareTo(c) < 0) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchTransactionD(ArrayList<Transaction> x, String c){
+        int low = 0;
+        int high = x.size() - 1;
+
+        while (high >= low) {
+            int middle = (low + high) / 2;
+            if (x.get(middle).getDate().compareTo(c) == 0) {
+                return middle;
+            } else if (x.get(middle).getDate().compareTo(c) < 0) {
                 low = middle + 1;
             } else {
                 high = middle - 1;
@@ -69,29 +84,29 @@ public final class Db {
             if (z == -1){
                 System.out.println("Transaction not found.");
             }else{
-                System.out.println(transactions[z]);
+                System.out.println(transactions.get(z));
             }
         }else if (x.charAt(0) == 'c'){
             System.out.println("Enter category: ");
             String y = input.nextLine();
 
-            int z = searchTransactionCD(transactions, y);
+            int z = searchTransactionC(transactions, y);
 
             if (z == -1){
                 System.out.println("Transaction not found.");
             }else{
-                System.out.println(transactions[z]);
+                System.out.println(transactions.get(z));
             }
         }else if (x.charAt(0) == 'd'){
             System.out.println("Enter date: ");
             String y = input.nextLine();
 
-            int z = searchTransactionCD(transactions, y);
+            int z = searchTransactionD(transactions, y);
 
             if (z == -1){
                 System.out.println("Transaction not found.");
             }else{
-                System.out.println(transactions[z]);
+                System.out.println(transactions.get(z));
             }
         }else{
             System.out.println("Enter a letter from the selection.");
